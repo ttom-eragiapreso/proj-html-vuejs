d284b67f25458beec0cd1e05abfced7ba20cd1c5
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-11-25 15:26:09 +0100

    finito who we are section

--- src/components/WhoWeAreComponent.vue
+++ src/components/WhoWeAreComponent.vue
@@ -9,13 +9,36 @@ export default {
 </script>
 
 <template>
+
   <div class="container">
     
     <div class="row">
 
       <div class="col col-9">
+
         <div class="who-we-are">
           <h5 class="text-primary">WHO WE ARE</h5>
+          
+          <span class="accent-text fs-1">
+            <font-awesome-icon icon="fa-regular fa-gem"/>
+            Excellence
+          </span>
+          <span class="fs-1 fw-bold ms-2">in Transport</span>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusantium ut soluta asperiores deleniti ratione alias, omnis, tenetur ullam saepe quos repudiandae praesentium quia eligendi, tempora quidem quaerat voluptate sed.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusantium ut soluta asperiores deleniti ratione alias, omnis, tenetur ullam saepe quos repudiandae praesentium quia eligendi, tempora quidem quaerat voluptate sed.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat accusantium ut soluta asperiores deleniti ratione alias, omnis, tenetur ullam saepe quos repudiandae praesentium quia eligendi, tempora quidem quaerat voluptate sed.</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente itaque repellendus repellat cupiditate?</p>
+
+          <ul>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Consectetur adipiscing elit</li>
+            <li>integer molestie lorem at masa</li>
+            <li>Facilissis in pretium nisl aliquet</li>
+            <li>Dapibus hendrerit mauris cuarbitur</li>
+          </ul>
+
         </div>
       </div>
       
@@ -35,6 +58,27 @@ export default {
 
   .container {
     height: 100vh;
+    padding: 7rem 0;
+  }
+
+  p {
+    color: $secondary;
+    &:first-of-type {
+      margin-top: 1rem;
+    }
+    &:nth-of-type(3){
+      // box-shadow: -5px 0px 0 0px $secondary-color;
+      border-left: 5px solid $secondary-color;
+      padding: 1rem 1.5rem;
+    }
+  }
+
+  ul {
+    list-style: disc;
+    padding-left: 1rem;
+    li{
+      color: $secondary;
+    }
   }
 
 </style>
